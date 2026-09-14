c31296c9f44e5c48ebc91a617209441e901fc169
Taheem Akter <taheem.akter@gmail.com>
2026-09-13 22:30:59 -0400

    Document Python SDK package exports

--- clients/python/taheem_ai/__init__.py
+++ clients/python/taheem_ai/__init__.py
@@ -1,5 +1,7 @@
-from taheem_ai.client import AI
+"""Public exports for the Local AI Gateway Python SDK."""
+
 from taheem_ai.async_client import AsyncAI
+from taheem_ai.client import AI
 from taheem_ai.errors import AIError
 
 __all__ = ["AI", "AsyncAI", "AIError"]
